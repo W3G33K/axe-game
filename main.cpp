7c776432d7182bd58c746169eab2af8e469d4d04
Ryan Clark <mail.w3geek@gmail.com>
2022-03-19 23:21:48 -0400

    Logical OR

--- main.cpp
+++ main.cpp
@@ -12,9 +12,10 @@ int main() {
 	int circle_y = (height / 2);
 	const int circle_radius = 32;
 
+	int axe_direction = 10;
 	int axe_x = 200;
 	int axe_y = 0;
-	const int axe_size = 64;
+	const int axe_size = 64;	
 
 	InitWindow(width, height, "Look ma, no hands!");
 	SetTargetFPS(60);
@@ -26,13 +27,16 @@ int main() {
 		DrawCircle(circle_x, circle_y, circle_radius, BLUE);
 		DrawRectangle(axe_x, axe_y, axe_size, axe_size, RED);
 
-		axe_y = (axe_y + 8);
+		axe_y = (axe_y + axe_direction);
+		if ((axe_y <= 0) || ((axe_y + axe_size) >= height)) {
+			axe_direction = -axe_direction;
+		}
 
-		if (IsKeyDown(KEY_A) && circle_x > (0 + circle_radius)) {
+		if (IsKeyDown(KEY_A) && circle_x >= (0 + circle_radius)) {
 			circle_x = (circle_x - 8);
 		}
 
-		if (IsKeyDown(KEY_D) && circle_x < (width - circle_radius)) {
+		if (IsKeyDown(KEY_D) && circle_x <= (width - circle_radius)) {
 			circle_x = (circle_x + 8);
 		}
 

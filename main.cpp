a234716c7b5c40b5de204bd1b72a389b515a57ca
Ryan Clark <mail.w3geek@gmail.com>
2022-03-19 22:50:43 -0400

    Drawing A Circle

--- main.cpp
+++ main.cpp
@@ -8,10 +8,17 @@ int main(int argc, char** argv) {
 	int width = 350;
 	int height = 200;
 
+	int centerX = (width / 2);
+	int centerY = (height / 2);
+
 	InitWindow(width, height, "Look ma, no hands!");
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(PURPLE);
+
+		DrawCircle(centerX, centerY, 32, BLUE);
+
 		EndDrawing();
 	}
 

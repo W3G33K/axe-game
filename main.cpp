d5004b721f760b592dec914bdf242552c53f0a47
Ryan Clark <mail.w3geek@gmail.com>
2022-03-19 22:12:55 -0400

    Debugging

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
+#include <stdio.h>
 #include "raylib.h"
 
 /**
  * External Libraries must be included with double quotes ("file.h") where as Standard Libraries must be included with angle brackets (<file>).
  **/
 
-int main(void) {
-	int width = 350;
-	int height = 200;
+int main(int argc, char* argv[]) {
+	int width = 350,
+		height = 200; // Variable Group Initialization & Declaration.
+	int center_x{ width / 2 }; // Braced Initialization.
+	int center_y{ height / 2 }; // Braced Initialization.
+	int center_z{}; // Braced Initialization (Declared & Initialized as 0).
 
+
+	printf("(x=%d, y=%d, z=%d)\n", center_x, center_y, center_z);
 	InitWindow(width, height, "Look ma, no hands!");
 	return 0;
 }

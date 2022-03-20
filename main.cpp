5776b0b2cf48a98dd7dea31f2d8a1ab394a389fc
Ryan Clark <mail.w3geek@gmail.com>
2022-03-19 22:38:54 -0400

    Clear Background

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
-#include <stdio.h>
 #include "raylib.h"
 
 /**
  * External Libraries must be included with double quotes ("file.h") where as Standard Libraries must be included with angle brackets (<file>).
  **/
 
-int main(int argc, char* argv[]) {
-	int width = 350,
-		height = 200; // Variable Group Initialization & Declaration.
-	int center_x{ width / 2 }; // Braced Initialization.
-	int center_y{ height / 2 }; // Braced Initialization.
-	int center_z{}; // Braced Initialization (Declared & Initialized as 0).
+int main(int argc, char** argv) {
+	int width = 350;
+	int height = 200;
 
-
-	printf("(x=%d, y=%d, z=%d)\n", center_x, center_y, center_z);
 	InitWindow(width, height, "Look ma, no hands!");
+	while (true) {
+		BeginDrawing();
+		ClearBackground(PURPLE);
+		EndDrawing();
+	}
+
 	return 0;
 }

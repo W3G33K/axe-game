15ca8a7550d415155c5686f16274f3f2ebfcf9ba
Ryan Clark <mail.w3geek@gmail.com>
2022-03-19 23:14:37 -0400

    Drawing The Axe

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
  **/
 
 int main() {
-	int width = 350;
-	int height = 200;
+	int width = 800;
+	int height = 450;
 
 	int circle_x = (width / 2);
 	int circle_y = (height / 2);
 	const int circle_radius = 32;
+
+	int axe_x = 200;
+	int axe_y = 0;
+	const int axe_size = 64;
 
 	InitWindow(width, height, "Look ma, no hands!");
 	SetTargetFPS(60);
@@ -20,6 +24,10 @@ int main() {
 		ClearBackground(PURPLE);
 
 		DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+		DrawRectangle(axe_x, axe_y, axe_size, axe_size, RED);
+
+		axe_y = (axe_y + 8);
+
 		if (IsKeyDown(KEY_A) && circle_x > (0 + circle_radius)) {
 			circle_x = (circle_x - 8);
 		}
